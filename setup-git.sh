924a33b24917be05c4bb9c8d5bb1d4caf2a980e6
Shin Saito <shinsa@jp.ibm.com>
2022-01-14 14:20:16 +0900

    update setup-git.sh

---
+++ setup-git.sh
@@ -0,0 +1,15 @@
+git config --global user.name "Shin Saito"
+git config --global ser.email shinsa@jp.ibm.com
+git config --global alias.s "status -sb --ignored"
+git config --global alias.c commit
+git config --global alias.p push
+git config --global alias.b "branch -a"
+git config --global alias.co "checkout"
+git config --global alias.f "fetch -p"
+git config --global alias.r "rebase origin/master"
+git config --global alias.d diff
+# git config --global url.git@github.ibm.com:.insteadof=https://github.ibm.com/
+# git config --global remind.paths=/Users/shinsa/git/*
+git config --global push.default current
+git config --global pull.rebase true
+# git config --global init.defaultbranch=main
